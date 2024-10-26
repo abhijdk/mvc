6dec48f0d9c559ac05ba61c488480a8c95b83537
abhin <abhinnabehuria@outlook.com>
2024-10-26 14:23:43 +0530

    Initial commit

---
+++ src/main/java/mycode/abhi/controller/DemoController.java
@@ -0,0 +1,12 @@
+package mycode.abhi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+    @RequestMapping("/demo")
+    public String demo(){
+        return "demo";
+    }
+}
